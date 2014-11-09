6c95376d67310c363866fe25c6736e7feb55f2fe
Paulo Tanimoto <ptanimoto@gmail.com>
2014-11-08 21:55:09 -0800

    Use a resource pool for managing workers

--- src/WatchIt.hs
+++ src/WatchIt.hs
@@ -25,6 +25,7 @@ import           WatchIt.Options
 import           Control.Concurrent        (threadDelay)
 import           Control.Monad             (forever, void)
 
+import           Data.Pool                 (Pool (..), createPool, tryWithResource)
 import           Data.Streaming.Process    (Inherited (..), shell, streamingProcess,
                                            waitForStreamingProcess)
 
@@ -32,7 +33,8 @@ import           Filesystem.Path.CurrentOS as FS
 
 import           Options.Applicative       (execParser)
 
-import           System.FSNotify           (eventPath, watchTree, withManager)
+import           System.FSNotify           (Event (..), eventPath, watchTree,
+                                           withManager)
 
 
 -------------------------------------------------------------------------------
@@ -48,22 +50,51 @@ watchIt :: Options -> IO ()
 watchIt opts = do
   let path = decodeString $ optionsPath opts
   let cmd = optionsCmd opts
+
+  -- Worker Pool
+  pool <- createWorkerPool
+
+  -- Watch it
+  putStrLn "watchit started..."
   withManager $ \man -> do
-    void $ watchTree man
-      path
+    void $ watchTree man path
       filterEvent
-      (const $ run cmd)
+      (handleEvent pool cmd)
     forever $ threadDelay longDelay
   where
-  ext = "hs"
-  filterEvent event = do
-    let file = eventPath event
-    hasExtension file ext
   longDelay = 12 * 3600 * 10000  -- maxBound
+
+
+filterEvent :: Event -> Bool
+filterEvent event = do
+  let file = eventPath event
+  let ext = "hs"
+  hasExtension file ext
+
+
+handleEvent :: Pool a -> String -> Event -> IO ()
+handleEvent pool cmd _ = do
+  void $ tryWithResource pool $ const
+       $ run cmd
+
+
+createWorkerPool :: IO (Pool ())
+createWorkerPool =
+  createPool
+    (return ())
+    (const $ return ())
+    stripes timeLeftOpen resPerStripe
+  where
+  stripes = 1
+  timeLeftOpen = 1
+  resPerStripe = 1
 
 
 run :: String -> IO ()
 run cmd = do
+  putStrLn ""
+  putStrLn "------------------------------------------------------------------------"
   (Inherited, Inherited, Inherited, handle) <-
     streamingProcess (shell cmd)
-  void $ waitForStreamingProcess handle
+  status <- waitForStreamingProcess handle
+  print status
